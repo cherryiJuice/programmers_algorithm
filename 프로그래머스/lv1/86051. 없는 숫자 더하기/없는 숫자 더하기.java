746f215fba7f67727e35e5504fcf5d9c073a0081
cherryiJuice <143402486+cherryiJuice@users.noreply.github.com>
2023-08-30 21:14:28 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] numbers) {
+        int sum = 45;
+        for (int i : numbers) {
+            sum -= i;
+        }
+        return sum;
+    }
+}
