be89e81134aa5b33a16f35a3fa4c5bf28dff9e8d
cherryiJuice <143402486+cherryiJuice@users.noreply.github.com>
2023-08-30 21:15:34 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,17 @@
+class Solution {
+    
+    public int solution(int num) {
+        int count=0;
+        long n=num;
+        while(n!=1){
+            if (count>=500){
+                count=-1;
+                break;
+            }
+            if(n%2==0) n /= 2;
+            else n = n*3 + 1;
+            count++;
+        }
+        return count;
+    }
+}
