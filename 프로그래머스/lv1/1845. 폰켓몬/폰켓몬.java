1da1c3ea1137a22206e0cf42aa888e51582d8463
cherryiJuice <143402486+cherryiJuice@users.noreply.github.com>
2023-08-30 21:16:40 +0900

    [level 1] Title: 폰켓몬, Time: 6.10 ms, Memory: 85.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] nums) {
+        int[] answer = Arrays.stream(nums).distinct().toArray();
+        if(answer.length>nums.length/2) return nums.length/2;
+        else return answer.length;
+    }
+}
