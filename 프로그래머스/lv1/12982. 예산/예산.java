e0a8eb3c18ec5cd52a16edf9db4edb89f35b0715
cherryiJuice <143402486+cherryiJuice@users.noreply.github.com>
2023-08-30 21:19:39 +0900

    [level 1] Title: 예산, Time: 0.32 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        int sum=0;
+        Arrays.sort(d);
+        for(int i=0; i<d.length; i++){
+            if(sum+d[i]<=budget){
+                sum+=d[i];
+                answer=i+1;
+            }
+            else break;  
+        }
+        return answer;
+    }
+}
